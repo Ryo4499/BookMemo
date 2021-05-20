8c2eacbcd2ff79d77a77ed7c16a3f71ab31410b2
Ryo4499 <ryo.p4499@gmail.com>
2021-05-21 00:00:43 +0900

    delete yagni package.

--- src/main/java/com/herokuapp/bookmemo4444/service/UserServiceImpl.java
+++ src/main/java/com/herokuapp/bookmemo4444/service/UserServiceImpl.java
@@ -3,7 +3,6 @@ package com.herokuapp.bookmemo4444.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.support.DaoSupport;
 import org.springframework.stereotype.Service;
 
 import com.herokuapp.bookmemo4444.dao.UserDao;
