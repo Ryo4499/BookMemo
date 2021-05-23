cfff0e0b7860540a04a221556f9a6f9063ef3f7e
Ryo4499 <ryo.p4499@gmail.com>
2021-05-24 08:51:38 +0900

    changed non null

--- src/main/java/com/herokuapp/bookmemo4444/user/SignupForm.java
+++ src/main/java/com/herokuapp/bookmemo4444/user/SignupForm.java
@@ -1,23 +1,22 @@
 package com.herokuapp.bookmemo4444.user;
 
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import lombok.NonNull;
 
 public class SignupForm {
-	@NotNull
+	@NonNull
 	@NotEmpty(message = "empty username")
 	@Pattern(regexp = "^[a-zA-Z0-9]([a-zA-Z0-9]?|[\\-]?([a-zA-Z0-9])){2,30}$")
 	private String userName;
 
-	@NotNull
+	@NonNull
 	@NotEmpty(message = "empty email")
 	@Pattern(regexp = "^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\\.)+[a-zA-Z]{2,128}$ ")
 	private String email;
 
-	@NotNull
+	@NonNull
 	@NotEmpty(message = "empty password")
 	@Pattern(regexp = "/^(?=.*?[a-z])(?=.*?\\d)[a-z\\d]{8,50}$/i")
 	private String password;
