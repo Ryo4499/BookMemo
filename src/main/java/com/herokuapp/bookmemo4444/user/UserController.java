fea78b9178d217df928f997d6993b8fde90197b1
Ryo4499 <ryo.p4499@gmail.com>
2021-06-07 09:56:42 +0900

    clear

--- src/main/java/com/herokuapp/bookmemo4444/user/UserController.java
+++ src/main/java/com/herokuapp/bookmemo4444/user/UserController.java
@@ -23,57 +23,25 @@ public class UserController {
 		return "top-page";
 	}
 
-//	@GetMapping("/login")
-//	public String getLoginPage() {
-//		return "user/login";
-//	}
-//
-//	@GetMapping("/signup")
-//	public String getSignupPage(SignupForm signupForm, Model model) {
-//		return "user/signup";
-//	}
-//
-//	@PostMapping("/signup")
-//	public String postSignup(@Validated SignupForm signupForm, BindingResult result, Model model,
-//			RedirectAttributes redirectAttributes) {
-//
-//		// TODO INS USER
-//		return "redirect:/memo/";
-//
-//	}
-//
-//	@GetMapping("/profile")
-//	public String getProfile(SignupForm signupForm, Model model) {
-//
-//		// TODO GET USER DATA
-//		return "user/user-profile";
-//	}
-//
-//	@PostMapping("/profile/update")
-//	public String postProfile(@Validated SignupForm signupForm, BindingResult result, Model model,
-//			RedirectAttributes redirectAttributes) {
-//
-//		return "user/user-profile";
-//
-//		// TODO USER UPDATE
-//		// return "redirect:/memo/";
-//
-//	}
-//
-//	@GetMapping("/delete")
-//	public String deleteUser(@Validated SignupForm signupForm, BindingResult result, Model model) {
-//		// TODO USER delete
-//		return "redirect:/";
-//	}
-//
-//	private User makeUser(SignupForm signupForm, Long userId, String sessionId) {
-//		User user = new User();
-//		if (userId != 0) {
-//			user.setUserId(userId);
-//		}
-//		user.setUserName(signupForm.getUserName());
-//		user.setUserEmail(signupForm.getEmail());
-//		user.setUserPassword(signupForm.getPassword());
-//		return user;
-//	}
+	// @GetMapping("/login")
+	// TODO public String getLoginPage() {
+
+	// @GetMapping("/signup")
+	// TODO public String getSignupPage(SignupForm signupForm, Model model) {
+
+	// @PostMapping("/signup")
+	// TODO public String postSignup(@Validated SignupForm signupForm, BindingResult
+	// result, Model model,RedirectAttributes redirectAttributes) {
+
+	// @GetMapping("/profile")
+	// TODO public String getProfile(SignupForm signupForm, Model model) {
+
+	// @PostMapping("/profile/update")
+	// TODO public String postProfile(@Validated SignupForm signupForm,
+	// BindingResult result, Model model,
+
+	//@GetMapping("/delete")
+	//TODO	public String deleteUser(@Validated SignupForm signupForm, BindingResult result, Model model) {
+	
+	//TODO	private User makeUser(SignupForm signupForm, Long userId, String sessionId) {
 }
