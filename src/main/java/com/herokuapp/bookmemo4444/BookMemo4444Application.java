1ae0802b01dc31a8b6ad2094579a0bf06234ac35
Ryo4499 <ryo.p4499@gmail.com>
2021-06-08 22:58:10 +0900

    changed jar

--- src/main/java/com/herokuapp/bookmemo4444/BookMemo4444Application.java
+++ src/main/java/com/herokuapp/bookmemo4444/BookMemo4444Application.java
@@ -8,12 +8,9 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class BookMemo4444Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookMemo4444Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BookMemo4444Application.class, args);
+  }
 
-	@Bean
-	public ServletContextInitializer servletContextInitializer() {
-		return servletContext -> servletContext.getSessionCookieConfig().setName("bookmemo.session");
-	}
 }
+
