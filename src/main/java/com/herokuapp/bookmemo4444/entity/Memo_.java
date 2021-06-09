719d7d9d510f23d4c23c7cb79c7f972d54883b8b
Ryo4499 <ryo.p4499@gmail.com>
2021-06-10 02:39:49 +0900

    mata model

---
+++ src/main/java/com/herokuapp/bookmemo4444/entity/Memo_.java
@@ -0,0 +1,17 @@
+package com.herokuapp.bookmemo4444.entity;
+
+import java.util.Date;
+
+import javax.persistence.metamodel.EntityType;
+import javax.persistence.metamodel.SingularAttribute;
+
+public class Memo_ {
+	public static volatile SingularAttribute<Memo, Long> memoId;
+	public static volatile SingularAttribute<Memo, String> title;
+	public static volatile SingularAttribute<Memo, String> content;
+	public static volatile SingularAttribute<Memo, String> category;
+	public static volatile SingularAttribute<Memo, String> bookName;
+	public static volatile SingularAttribute<Memo, Account> account;
+	public static volatile SingularAttribute<Memo, Date> createdDate;
+	public static volatile SingularAttribute<Memo, Date> updatedDate;
+}
---
+++ src/main/java/com/herokuapp/bookmemo4444/entity/Role_.java
@@ -0,0 +1,10 @@
+package com.herokuapp.bookmemo4444.entity;
+
+import javax.persistence.metamodel.SetAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+
+public class Role_ {
+	public static volatile SingularAttribute<Role, Long> id;
+	public static volatile SingularAttribute<Role, String> authority;
+	public static volatile SetAttribute<Role, Account> accounts;
+}
