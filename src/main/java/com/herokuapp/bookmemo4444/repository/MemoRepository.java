830d7ef3fbd90994483f3fa9eef92207f5d6818f
Ryo4499 <ryo.p4499@gmail.com>
2021-06-11 01:32:35 +0900

    add delete admin

--- src/main/java/com/herokuapp/bookmemo4444/repository/MemoRepository.java
+++ src/main/java/com/herokuapp/bookmemo4444/repository/MemoRepository.java
@@ -1,12 +1,14 @@
 package com.herokuapp.bookmemo4444.repository;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,9 +22,14 @@ public interface MemoRepository extends JpaRepository<Memo, Long> {
 	@Query("select m.createdDate from Memo m where m.memoId = ?1")
 	Date findByMemoId(Long memoId);
 
-	@Modifying(clearAutomatically=true, flushAutomatically=true)
+	@Modifying(clearAutomatically = true, flushAutomatically = true)
 	@Transactional(readOnly = false)
-	@Query(value="Delete FROM memos where account_id = :id",nativeQuery = true)
+	@Query(value = "Delete FROM memos where account_id = :id", nativeQuery = true)
 	void deleteByAccount(@Param("id") Long accountId);
-	
+
+	@Secured("ROLE_ADMIN")
+	@Transactional(readOnly = true)
+	@Query("select m from Memo m where m.account = ?1")
+	List<Memo> findByAccountId(Long accountId);
+
 }
