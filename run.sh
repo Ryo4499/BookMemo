80b526c61e670796bc4d6fb3db07256657bfa5d1
Ryo4499 <ryo.p4499@gmail.com>
2023-02-26 23:53:05 +0900

    Modify: run command

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-# .env
-source .env
-docker run --name bookmemodb -e POSTGRES_USER=$POSTGRES_USER -e POSTGRES_DB=POSTGRES_DB -e POSTGRES_PASSWORD=POSTGRES_PASSWORD -p 5432:5432 -td postgres:14
 docker build . -t bookmemo
 docker run --name bookmemoapp -e POSTGRES_USER=$POSTGRES_USER -e POSTGRES_DB=POSTGRES_DB -e POSTGRES_PASSWORD=POSTGRES_PASSWORD -e DB_URL=DB_URL -td bookmemo
